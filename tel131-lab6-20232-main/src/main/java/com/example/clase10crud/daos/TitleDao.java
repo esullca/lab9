882456117deff172c39ef620d6549d364ce46e7c
AlliPaDi <148405450+AlliPaDi@users.noreply.github.com>
2023-10-29 12:01:39 -0500

    Add files via upload

--- tel131-lab6-20232-main/src/main/java/com/example/clase10crud/daos/TitleDao.java
+++ tel131-lab6-20232-main/src/main/java/com/example/clase10crud/daos/TitleDao.java
@@ -45,4 +45,33 @@ public class TitleDao {
 
         return lista;
     }
+
+    public void crear(int titleEmp_no, String titleTitle, String titleFromDate, String titleToDate){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/hr";
+        String username = "root";
+        String password = "root";
+
+        String sql = "insert into titles (emp_no, title, from_date,to_date) values (?,?,?,?)";
+
+        try(Connection connection = DriverManager.getConnection(url,username,password);
+            PreparedStatement pstmt = connection.prepareStatement(sql)){
+
+            pstmt.setInt(1,titleEmp_no);
+            pstmt.setString(2,titleTitle);
+            pstmt.setString(3,titleFromDate);
+            pstmt.setString(4,titleToDate);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
