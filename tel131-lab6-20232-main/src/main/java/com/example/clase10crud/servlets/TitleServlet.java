888ae7b3f8a3db6769f6efebdc67a633db23eb88
AlliPaDi <148405450+AlliPaDi@users.noreply.github.com>
2023-10-29 12:00:34 -0500

    Add files via upload

--- tel131-lab6-20232-main/src/main/java/com/example/clase10crud/servlets/TitleServlet.java
+++ tel131-lab6-20232-main/src/main/java/com/example/clase10crud/servlets/TitleServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 @WebServlet(name = "TitleServlet", value = "/TitleServlet")
@@ -18,19 +19,71 @@ public class TitleServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
-
+        String action = request.getParameter("action") == null? "lista":request.getParameter("action");
         TitleDao titleDao = new TitleDao();
-        ArrayList<Title> list = titleDao.list();
 
-        //mandar la lista a la vista -> job/lista.jsp
-        request.setAttribute("lista",list);
-        RequestDispatcher rd = request.getRequestDispatcher("title/lista.jsp");
-        rd.forward(request,response);
+        switch (action){
+            case "lista":
+                ArrayList<Title> list = titleDao.list();
+                //mandar la lista a la vista -> job/lista.jsp
+                request.setAttribute("lista",list);
+                RequestDispatcher rd = request.getRequestDispatcher("title/lista.jsp");
+                rd.forward(request,response);
+
+
+                break;
+            case "new":
+                request.getRequestDispatcher("title/form_new.jsp").forward(request,response);
+                break;
+            case "del":
+                String idd = request.getParameter("id");
+                Title title = TitleDao.buscarPorId(idd);
+
+                if(title != null){
+                    try {
+                        titleDao.borrar(idd);
+                    } catch (SQLException e) {
+                        System.out.println("Log: excepcion: " + e.getMessage());
+                    }
+                }
+                response.sendRedirect(request.getContextPath() + "/JobServlet");
+                break;
+
+        }
+
+
         //TODO: complete
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        TitleDao titleDao = new TitleDao();
+        String action = request.getParameter("action") == null? "crear":request.getParameter("action");
+
+        switch (action){
+            case "crear":
+                String titleEmp_no = request.getParameter("titleEmp_no");
+                String titleTitle = request.getParameter("titleTitle");
+                String titleFromDate = request.getParameter("titleFromDate");
+                String titleToDate = request.getParameter("titleToDate");
+
+                boolean isAllValid = true;
+
+                if(titleTitle.length()>50){
+                    isAllValid = false;
+                }
+
+                if(isAllValid){
+                    TitleDao.crear(titleEmp_no, titleTitle, titleFromDate,titleToDate);
+                }else{
+                    request.getRequestDispatcher("title/form_new.jsp").forward(request,response);
+                }
+                break;
+
+        }
+
+
     }
 }
